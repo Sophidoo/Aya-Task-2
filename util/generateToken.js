03cbccee28b8d2c6c5e895d088e3ea57ad79d756
Sophia Okosodo <sophieokosodo@gmail.com>
2023-01-31 16:19:52 +0100

    error

--- util/generateToken.js
+++ util/generateToken.js
@@ -1,7 +1,7 @@
 import jwt from "jsonwebtoken";
 
-const generateToken = (token) => {
-    return jwt.sign({token}, process.env.TOKEN_KEY, {expiresIn: process.env.TOKEN_EXPIRES})
+const generateToken = (id) => {
+    return jwt.sign({id}, process.env.TOKEN_KEY, {expiresIn: process.env.TOKEN_EXPIRES})
 }
 
 export default generateToken;
