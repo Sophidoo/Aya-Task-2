715a749ebdd8adc0a23809249291c6d6eb41a1c3
Sophia Okosodo <sophieokosodo@gmail.com>
2023-01-31 10:48:23 +0100

    added cors

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ import express from "express";
 import { dbConnect } from "./config/dbConnect.js";
 import dotenv from "dotenv";
 import userRoute from "./routes/userRoutes.js";
+import cors from "cors"
 
 dotenv.config()
 dbConnect();
@@ -12,14 +13,14 @@ app.use(express.json());
 
 app.use("/api/v1/users", userRoute);
 
-const cors=require("cors");
-const corsOptions ={
-   origin:'*', 
-   credentials:true,            //access-control-allow-credentials:true
-   optionSuccessStatus:200,
-}
+// const cors=require("cors");
+// const corsOptions ={
+//    origin:'*', 
+//    credentials:true,            //access-control-allow-credentials:true
+//    optionSuccessStatus:200,
+// }
 
-app.use(cors(corsOptions)) // Use this after the variable declaration
+app.use(cors()) // Use this after the variable declaration
 
-const PORT = process.env.PORT || 3100
+const PORT = process.env.PORT
 app.listen(PORT, console.log(`Server is running at ${PORT}`))
