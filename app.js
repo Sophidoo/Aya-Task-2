01e0aea84749399b669c51c43173440f8cd25f3d
Sophia Okosodo <sophieokosodo@gmail.com>
2023-01-31 10:31:58 +0100

    added cors

--- app.js
+++ app.js
@@ -12,5 +12,14 @@ app.use(express.json());
 
 app.use("/api/v1/users", userRoute);
 
+const cors=require("cors");
+const corsOptions ={
+   origin:'*', 
+   credentials:true,            //access-control-allow-credentials:true
+   optionSuccessStatus:200,
+}
+
+app.use(cors(corsOptions)) // Use this after the variable declaration
+
 const PORT = process.env.PORT || 3100
 app.listen(PORT, console.log(`Server is running at ${PORT}`))
